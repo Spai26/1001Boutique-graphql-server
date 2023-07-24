8e8c3e55668925c75b5acc6585db5b992ec7338d
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-07-24 12:01:43 -0500

    separando configuracion de express

---
+++ src/config/server.ts
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import morgan from 'morgan';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import { corsOptions } from '@libs/corsOptions';
+import express, { Express } from 'express';
+
+export const app: Express = express();
+
+//configuracion de express
+
+app.use(morgan('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(compression());
