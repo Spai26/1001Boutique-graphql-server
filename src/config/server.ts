b423ce23596fd5df3ebea17d37fcbd0a5460413d
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-08-29 20:12:11 -0500

    -

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,5 +1,4 @@
 import cors from 'cors';
-import morgan from 'morgan';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import { corsOptions } from '@libs/corsOptions';
@@ -9,9 +8,6 @@ export const app: Express = express();
 
 // configuracion de express
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev')); // Usar 'dev' solo en desarrollo
-}
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
